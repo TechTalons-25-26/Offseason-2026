c5d5b6896afd0b9162a7772a10662933ceb806f1
shruthi-l09 <saishruthilaks@gmail.com>
2026-07-28 15:15:11 -0700

    Claw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sampleTeleOp/sampleLinearOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sampleTeleOp/sampleLinearOpMode.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+
 @TeleOp(name = "sampleLinearOpMode")
 public class sampleLinearOpMode extends LinearOpMode {
 
